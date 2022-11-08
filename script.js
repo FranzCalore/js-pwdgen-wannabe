a64ee4f4f91b36062f9643235505b0743079c0a7
FranzCalore <franzkaiser1993@gmail.com>
2022-11-08 14:15:18 +0100

    Completata la parte di Javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+/* let variabileNome = prompt("inserisci il tuo nome");
+let variabileCognome = prompt ("inserisci il tuo cognome");
+let variabileColore = prompt ("inserisci il tuo colore preferito");
+let variabileNumero1 = prompt ("inserisci un numero");
+let variabileNumero2 = prompt ("inserisci un altro numero");
+
+variabileNumero1 = parseInt(variabileNumero1);
+variabileNumero2 = parseInt(variabileNumero2);
+
+let variabileQuoziente = variabileNumero1/variabileNumero2;
+variabileQuoziente = variabileQuoziente.toFixed(1)
+
+let passwordSicurissima = variabileNome + variabileCognome + variabileColore +variabileQuoziente;
+
+console.info (passwordSicurissima) */
